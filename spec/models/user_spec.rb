3983f837d301ba5b5704d0924d5716e2eea06bec
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-08 15:35:34 -0600

    Pass test for user has many pets, test user pet relationship

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe User, type: :model do
   describe "vaildations" do
     it {should validate_presence_of(:name)}
   end
+  describe "relationsships" do
+    it {should have_many(:pets)}
+  end
 end
