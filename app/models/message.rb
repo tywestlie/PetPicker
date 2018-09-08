b767c673844e2f7a06067cbf0147a48a9dead534
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-08 16:23:07 -0600

    Adds message belongs to sender in connection

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
-    validates_presence_of :body
-
+  validates_presence_of :body
+  belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
+  belongs_to :connection
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Message, type: :model do
   describe "validations" do
     it {should validate_presence_of(:body)}
   end
+  describe "relationships" do
+    it {should belong_to(:sender)}
+    it {should belong_to(:connection)}
+  end
 end
