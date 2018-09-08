7a9920f171d14e5370d92de9e19d5300f9c633ee
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-08 16:16:54 -0600

    Connection has many messages

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,3 +1,7 @@
 class Connection < ApplicationRecord
   validates_presence_of :status
+
+  belongs_to :adopter, class_name: 'User', foreign_key: 'adopter_id'
+  belongs_to :pet
+  has_many :messages, dependent: :delete_all
 end
--- spec/models/connection_spec.rb
+++ spec/models/connection_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Connection, type: :model do
   describe "validations" do
     it {should validate_presence_of(:status)}
   end
+  describe "relationships" do
+    it {should belong_to(:adopter)}
+    it {should belong_to(:pet)}
+    it {should have_many(:messages)}
+  end
 end
