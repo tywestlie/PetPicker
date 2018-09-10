4688741b3f0fe8f00ad6f458d902289c3799fc15
apoc64 <sschwedt@gmail.com>
2018-09-10 16:31:38 -0600

    Adds secure password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   validates_presence_of :name
+  has_secure_password
+
   has_many :pets, dependent: :delete_all
   has_many :connections, class_name: 'Connection', foreign_key: 'adopter_id', dependent: :delete_all
   has_many :messages, class_name: 'Message', foreign_key: 'sender_id', dependent: :delete_all
