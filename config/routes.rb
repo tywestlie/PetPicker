e02e16983ae7eae7d57388dd80ffd48c3f5947b1
Tyler Westlie <tyler.westlie@gmail.com>
2018-09-11 13:57:34 -0600

    adds pet to a user, passing tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
-  namespace :v1 do
-    resources :users, only: [:create]
+    namespace :v1 do
+      resources :users, only: [:create]
+      namespace :users do
+        post '/:id/pets', to: 'pets#create'
+      end
+    end
   end
-end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,6 @@
 FactoryBot.define do
   factory :user do
+    name Faker::Simpsons.unique.character
+    password Faker::Crypto.sha256
   end
 end
