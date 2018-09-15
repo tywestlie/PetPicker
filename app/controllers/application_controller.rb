f38680d22ced067cc3f6d49380ac92caec2fd4b4
Tyler Westlie <tyler.westlie@gmail.com>
2018-09-15 15:31:16 -0600

    tests sad path for delete user, adds 404 not found for resource that doesnt exist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+private
+  def record_not_found
+    render json: {message: "Not Found"}, status: 404
+  end
 end
