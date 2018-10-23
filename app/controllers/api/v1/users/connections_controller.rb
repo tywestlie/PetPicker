4e5bd7f95c94db06aeb9f00b5cc5a8ff521fbb89
Tyler Westlie <tyler.westlie@gmail.com>
2018-10-23 15:23:00 -0600

    removes puts statements from the connection controller and model

--- app/controllers/api/v1/users/connections_controller.rb
+++ app/controllers/api/v1/users/connections_controller.rb
@@ -3,22 +3,12 @@ class Api::V1::Users::ConnectionsController < ApplicationController
     pet = Pet.find(params[:pet_id])
     name = pet.name
     connection = Connection.create(pet_id: pet.id, adopter_id: params[:id], status: "like")
-    if connection.persisted?
-      connection.success
-      render json: {message: "You liked #{pet.name}"}
-    else
-      connection.failure
-    end
+    render json: {message: "You liked #{pet.name}"}
   end
 
   def destroy
     pet = Pet.find(params[:pet_id])
     connection = Connection.create(pet_id: pet.id, adopter_id: params[:id], status: "nope")
-    if connection.persisted?
-      connection.success
-      render json: {message: "You noped #{pet.name}"}
-    else
-      connection.failure
-    end
+    render json: {message: "You noped #{pet.name}"}
   end
 end
--- app/models/connection.rb
+++ app/models/connection.rb
@@ -6,13 +6,4 @@ class Connection < ApplicationRecord
   belongs_to :adopter, class_name: 'User', foreign_key: 'adopter_id'
   belongs_to :pet
   has_many :messages, dependent: :delete_all
-
-  def success
-    puts "Created connection #{id} with status #{status} for pet #{pet.name} and user #{adopter.name}"
-  end
-
-  def failure
-    puts "DID NOT SAVE connection #{id} with status #{status} for pet #{pet.name} and user #{adopter.name}"
-  end
-
 end
