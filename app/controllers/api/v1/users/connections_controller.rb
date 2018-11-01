44fee7b15a6d90c1841b5e832c87cf313b2e7668
apoc64 <sschwedt@gmail.com>
2018-11-01 13:47:20 -0600

    Removes unnecesary conditional logic in connections_controller, improves testing

--- app/controllers/api/v1/users/connections_controller.rb
+++ app/controllers/api/v1/users/connections_controller.rb
@@ -3,22 +3,13 @@ class Api::V1::Users::ConnectionsController < ApplicationController
 
   def create
     pet = Pet.find(params[:pet_id])
-    name = pet.name
-    connection = Connection.create(pet_id: pet.id, adopter_id: params[:id], status: "like")
-    if connection.persisted?
-      render json: {message: "You liked #{pet.name}"}
-    else
-      render json: {message: "Failed to like pet"}
-    end
+    Connection.create(pet_id: pet.id, adopter_id: params[:id], status: "like")
+    render json: {message: "You liked #{pet.name}"}
   end
 
   def destroy
     pet = Pet.find(params[:pet_id])
-    connection = Connection.create(pet_id: pet.id, adopter_id: params[:id], status: "nope")
-    if connection.persisted?
-      render json: {message: "You noped #{pet.name}"}
-    else
-      render json: {message: "Failed to nope pet"}
-    end
+    Connection.create(pet_id: pet.id, adopter_id: params[:id], status: "nope")
+    render json: {message: "You noped #{pet.name}"}
   end
 end
--- spec/requests/adopter_connects_to_a_pet_spec.rb
+++ spec/requests/adopter_connects_to_a_pet_spec.rb
@@ -9,6 +9,8 @@ describe "Adopter likes a pet" do
     post "/api/v1/users/#{user.id}/connections", params: {pet_id: pet.id}
 
     expect(response).to_not be_successful
+    body = JSON.parse(response.body)
+    expect(body["message"]).to eq("invalid token")
   end
 
   it 'creates a like connection' do
@@ -28,6 +30,19 @@ describe "Adopter likes a pet" do
     expect(conn.status).to eq("like")
     expect(body["message"]).to eq("You liked #{pet.name}")
   end
+
+  it 'doesnt create a like connection with invalid pet' do
+    user = create(:user)
+    token = JWT.encode user.id, Rails.application.secret_key_base, 'HS256'
+    user2 = create(:user)
+    pet = create(:pet, user: user2)
+
+    post "/api/v1/users/#{user.id}/connections", params: {pet_id: (pet.id + 1), token: token}
+
+    expect(response).to_not be_successful
+    body = JSON.parse(response.body)
+    expect(body["message"]).to eq("Not Found")
+  end
 end
 
 describe "Adopter nopes a pet" do
@@ -39,6 +54,8 @@ describe "Adopter nopes a pet" do
     delete "/api/v1/users/#{user.id}/connections", params: {pet_id: pet.id}
 
     expect(response).to_not be_successful
+    body = JSON.parse(response.body)
+    expect(body["message"]).to eq("invalid token")
   end
 
   it 'creates a nope connection' do
@@ -57,5 +74,18 @@ describe "Adopter nopes a pet" do
     expect(conn.adopter_id).to eq(user.id)
     expect(conn.status).to eq("nope")
     expect(body["message"]).to eq("You noped #{pet.name}")
+  end
+
+  it 'doesnt create a nope connection with invalid pet' do
+    user = create(:user)
+    token = JWT.encode user.id, Rails.application.secret_key_base, 'HS256'
+    user2 = create(:user)
+    pet = create(:pet, user: user2)
+
+    delete "/api/v1/users/#{user.id}/connections", params: {pet_id: (pet.id + 1), token: token}
+
+    expect(response).to_not be_successful
+    body = JSON.parse(response.body)
+    expect(body["message"]).to eq("Not Found")
   end
 end
