e307df91bca77b8b3f8f3cf45c4988349e2b5700
apoc64 <sschwedt@gmail.com>
2018-11-06 12:37:46 -0700

    Changes double quotes to single quotes

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,13 +4,13 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :connections, dependent: :destroy
 
-  def self.find_pets(id)
+  def self.find_pets(user_id)
     find_by_sql("
       SELECT pets.* FROM pets
       LEFT OUTER JOIN connections on connections.pet_id=pets.id
       EXCEPT (SELECT pets.* from pets
         LEFT OUTER JOIN connections on connections.pet_id=pets.id
-        WHERE connections.adopter_id = #{id} OR pets.user_id = #{id})
+        WHERE connections.adopter_id = #{user_id} OR pets.user_id = #{user_id})
       LIMIT 10")
   end
 end
