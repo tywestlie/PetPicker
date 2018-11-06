5c32b542f82dc97a8527624be4713e502eddb237
apoc64 <sschwedt@gmail.com>
2018-11-06 12:21:11 -0700

    Improves syntax on application_controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,39 +1,41 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  private
+
   def authenticate_user(user, pass)
-    if user && user.authenticate(pass)
-      render json: user
-    else
-      render json: { 'message': 'login failed' }, status: 404
-    end
+    return render json: user if authenticate(user, pass)
+    login_failure
+  end
+
+  def authenticate(user, pass)
+    !user.nil? && user.authenticate(pass)
   end
 
   def authenticate_role(user, role)
-    if role  == 'owner'
-      user.role = 'owner'
-    else
-      user.role = 'adopter'
-    end
-  end
-
-  def authenticate_token
-    user = User.find(params['id'])
-    actual_token = JWT.encode user.id, Rails.application.secret_key_base, 'HS256'
-    params['token'] == actual_token
-  end
-
-  private
-
-  def record_not_found
-    render json: {message: 'Not Found'}, status: 404
-  end
-
-  def invalid_token
-    render json: {message: 'invalid token'}, status: 400
+    return user.role = 'owner' if role == 'owner'
+    user.role = 'adopter'
   end
 
   def validate_token
     return invalid_token unless authenticate_token
+  end
+
+  def authenticate_token
+    user = User.find(params['id'])
+    token = JWT.encode user.id, Rails.application.secret_key_base, 'HS256'
+    params['token'] == token
+  end
+
+  def invalid_token
+    render json: { message: 'invalid token' }, status: 400
+  end
+
+  def record_not_found
+    render json: { message: 'Not Found' }, status: 404
+  end
+
+  def login_failure
+    render json: { message: 'login failed' }, status: 400
   end
 end
